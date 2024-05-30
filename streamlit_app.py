a8a2fa8b959d41cbfea799721c625daab92529f6
gouthami24 <gouthami.santhanam@gmail.com>
2024-05-29 22:47:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,14 @@ import sqlite3
 import pandas as pd
 import requests
 import streamlit as st
+import re
+import numpy as np
+import openai
+import sqlite3
+import pytz
+from datetime import date, datetime
+from typing import Optional
+from langchain_core.runnables import ensure_config
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain.agents import create_openai_functions_agent, AgentExecutor
@@ -16,6 +24,14 @@ from langchain.chains import create_retrieval_chain
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain.tools.retriever import create_retriever_tool
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.tools import tool
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import AnyMessage, add_messages
+from langchain_anthropic import ChatAnthropic
+from langchain_core.runnables import Runnable, RunnableConfig
+
+
 
 st.title("Customer Support Bot")
 
@@ -104,5 +120,321 @@ conn.commit()
 conn.close()
 
 db = local_file  # We'll be using this local file as our DB in this tutorial
-stringio = StringIO(db.getvalue().decode("utf-8"))
-st.text_area("File Content", stringio.read())
+
+####
+response = requests.get(
+    "https://storage.googleapis.com/benchmarks-artifacts/travel-db/swiss_faq.md"
+)
+response.raise_for_status()
+faq_text = response.text
+
+docs = [{"page_content": txt} for txt in re.split(r"(?=\n##)", faq_text)]
+
+
+class VectorStoreRetriever:
+    def __init__(self, docs: list, vectors: list, oai_client):
+        self._arr = np.array(vectors)
+        self._docs = docs
+        self._client = oai_client
+
+    @classmethod
+    def from_docs(cls, docs, oai_client):
+        embeddings = oai_client.embeddings.create(
+            model="text-embedding-3-small", input=[doc["page_content"] for doc in docs]
+        )
+        vectors = [emb.embedding for emb in embeddings.data]
+        return cls(docs, vectors, oai_client)
+
+    def query(self, query: str, k: int = 5) -> list[dict]:
+        embed = self._client.embeddings.create(
+            model="text-embedding-3-small", input=[query]
+        )
+        # "@" is just a matrix multiplication in python
+        scores = np.array(embed.data[0].embedding) @ self._arr.T
+        top_k_idx = np.argpartition(scores, -k)[-k:]
+        top_k_idx_sorted = top_k_idx[np.argsort(-scores[top_k_idx])]
+        return [
+            {**self._docs[idx], "similarity": scores[idx]} for idx in top_k_idx_sorted
+        ]
+
+
+retriever = VectorStoreRetriever.from_docs(docs, openai.Client())
+
+
+@tool
+def lookup_policy(query: str) -> str:
+    """Consult the company policies to check whether certain options are permitted.
+    Use this before making any flight changes performing other 'write' events."""
+    docs = retriever.query(query, k=2)
+    return "\n\n".join([doc["page_content"] for doc in docs])
+
+
+## Flights
+@tool
+def fetch_user_flight_information() -> list[dict]:
+    """Fetch all tickets for the user along with corresponding flight information and seat assignments.
+
+    Returns:
+        A list of dictionaries where each dictionary contains the ticket details,
+        associated flight details, and the seat assignments for each ticket belonging to the user.
+    """
+    config = ensure_config()  # Fetch from the context
+    configuration = config.get("configurable", {})
+    passenger_id = configuration.get("passenger_id", None)
+    if not passenger_id:
+        raise ValueError("No passenger ID configured.")
+
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = """
+    SELECT 
+        t.ticket_no, t.book_ref,
+        f.flight_id, f.flight_no, f.departure_airport, f.arrival_airport, f.scheduled_departure, f.scheduled_arrival,
+        bp.seat_no, tf.fare_conditions
+    FROM 
+        tickets t
+        JOIN ticket_flights tf ON t.ticket_no = tf.ticket_no
+        JOIN flights f ON tf.flight_id = f.flight_id
+        JOIN boarding_passes bp ON bp.ticket_no = t.ticket_no AND bp.flight_id = f.flight_id
+    WHERE 
+        t.passenger_id = ?
+    """
+    cursor.execute(query, (passenger_id,))
+    rows = cursor.fetchall()
+    column_names = [column[0] for column in cursor.description]
+    results = [dict(zip(column_names, row)) for row in rows]
+
+    cursor.close()
+    conn.close()
+
+    return results
+
+
+@tool
+def search_flights(
+    departure_airport: Optional[str] = None,
+    arrival_airport: Optional[str] = None,
+    start_time: Optional[date | datetime] = None,
+    end_time: Optional[date | datetime] = None,
+    limit: int = 20,
+) -> list[dict]:
+    """Search for flights based on departure airport, arrival airport, and departure time range."""
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM flights WHERE 1 = 1"
+    params = []
+
+    if departure_airport:
+        query += " AND departure_airport = ?"
+        params.append(departure_airport)
+
+    if arrival_airport:
+        query += " AND arrival_airport = ?"
+        params.append(arrival_airport)
+
+    if start_time:
+        query += " AND scheduled_departure >= ?"
+        params.append(start_time)
+
+    if end_time:
+        query += " AND scheduled_departure <= ?"
+        params.append(end_time)
+    query += " LIMIT ?"
+    params.append(limit)
+    cursor.execute(query, params)
+    rows = cursor.fetchall()
+    column_names = [column[0] for column in cursor.description]
+    results = [dict(zip(column_names, row)) for row in rows]
+
+    cursor.close()
+    conn.close()
+
+    return results
+
+
+@tool
+def update_ticket_to_new_flight(ticket_no: str, new_flight_id: int) -> str:
+    """Update the user's ticket to a new valid flight."""
+    config = ensure_config()
+    configuration = config.get("configurable", {})
+    passenger_id = configuration.get("passenger_id", None)
+    if not passenger_id:
+        raise ValueError("No passenger ID configured.")
+
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT departure_airport, arrival_airport, scheduled_departure FROM flights WHERE flight_id = ?",
+        (new_flight_id,),
+    )
+    new_flight = cursor.fetchone()
+    if not new_flight:
+        cursor.close()
+        conn.close()
+        return "Invalid new flight ID provided."
+    column_names = [column[0] for column in cursor.description]
+    new_flight_dict = dict(zip(column_names, new_flight))
+    timezone = pytz.timezone("Etc/GMT-3")
+    current_time = datetime.now(tz=timezone)
+    departure_time = datetime.strptime(
+        new_flight_dict["scheduled_departure"], "%Y-%m-%d %H:%M:%S.%f%z"
+    )
+    time_until = (departure_time - current_time).total_seconds()
+    if time_until < (3 * 3600):
+        return f"Not permitted to reschedule to a flight that is less than 3 hours from the current time. Selected flight is at {departure_time}."
+
+    cursor.execute(
+        "SELECT flight_id FROM ticket_flights WHERE ticket_no = ?", (ticket_no,)
+    )
+    current_flight = cursor.fetchone()
+    if not current_flight:
+        cursor.close()
+        conn.close()
+        return "No existing ticket found for the given ticket number."
+
+    # Check the signed-in user actually has this ticket
+    cursor.execute(
+        "SELECT * FROM tickets WHERE ticket_no = ? AND passenger_id = ?",
+        (ticket_no, passenger_id),
+    )
+    current_ticket = cursor.fetchone()
+    if not current_ticket:
+        cursor.close()
+        conn.close()
+        return f"Current signed-in passenger with ID {passenger_id} not the owner of ticket {ticket_no}"
+
+    # In a real application, you'd likely add additional checks here to enforce business logic,
+    # like "does the new departure airport match the current ticket", etc.
+    # While it's best to try to be *proactive* in 'type-hinting' policies to the LLM
+    # it's inevitably going to get things wrong, so you **also** need to ensure your
+    # API enforces valid behavior
+    cursor.execute(
+        "UPDATE ticket_flights SET flight_id = ? WHERE ticket_no = ?",
+        (new_flight_id, ticket_no),
+    )
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    return "Ticket successfully updated to new flight."
+
+
+@tool
+def cancel_ticket(ticket_no: str) -> str:
+    """Cancel the user's ticket and remove it from the database."""
+    config = ensure_config()
+    configuration = config.get("configurable", {})
+    passenger_id = configuration.get("passenger_id", None)
+    if not passenger_id:
+        raise ValueError("No passenger ID configured.")
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT flight_id FROM ticket_flights WHERE ticket_no = ?", (ticket_no,)
+    )
+    existing_ticket = cursor.fetchone()
+    if not existing_ticket:
+        cursor.close()
+        conn.close()
+        return "No existing ticket found for the given ticket number."
+
+    # Check the signed-in user actually has this ticket
+    cursor.execute(
+        "SELECT flight_id FROM tickets WHERE ticket_no = ? AND passenger_id = ?",
+        (ticket_no, passenger_id),
+    )
+    current_ticket = cursor.fetchone()
+    if not current_ticket:
+        cursor.close()
+        conn.close()
+        return f"Current signed-in passenger with ID {passenger_id} not the owner of ticket {ticket_no}"
+
+    cursor.execute("DELETE FROM ticket_flights WHERE ticket_no = ?", (ticket_no,))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    return "Ticket successfully cancelled."
+
+
+##### state
+
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+
+
+  #### agents
+
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            passenger_id = config.get("passenger_id", None)
+            state = {**state, "user_info": passenger_id}
+            result = self.runnable.invoke(state)
+            # If the LLM happens to return an empty response, we will re-prompt it
+            # for an actual response.
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+
+
+# Haiku is faster and cheaper, but less accurate
+# llm = ChatAnthropic(model="claude-3-haiku-20240307")
+llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=1)
+# You could swap LLMs, though you will likely want to update the prompts when
+# doing so!
+# from langchain_openai import ChatOpenAI
+
+# llm = ChatOpenAI(model="gpt-4-turbo-preview")
+
+primary_assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Swiss Airlines. "
+            " Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now())
+
+part_1_tools = [
+    TavilySearchResults(max_results=1),
+    fetch_user_flight_information,
+    search_flights,
+    lookup_policy,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+]
+part_1_assistant_runnable = primary_assistant_prompt | llm.bind_tools(part_1_tools)
